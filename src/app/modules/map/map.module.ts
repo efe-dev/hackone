42e808ef72c7b21dc4b3227872665a00047a4067
Marcin Imielski <mimielski@syncad.com>
2019-03-23 14:31:09 +0100

    init agm

--- src/app/modules/map/map.module.ts
+++ src/app/modules/map/map.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { MapService } from './map.service';
 import { MapDirective } from './map.directive';
 import { MapComponent } from './map.component';
+import { AgmCoreModule } from '@agm/core';
+import { environment } from '../../../environments/environment';
 
 
 @NgModule({
   declarations: [MapDirective, MapComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.mapsApiKey
+    })],
   providers: [MapService],
   exports: [MapDirective, MapComponent]
 })
-export class MapModule {}
+export class
+ MapModule {}
--- src/app/modules/map/map.service.ts
+++ src/app/modules/map/map.service.ts
@@ -1,46 +1,8 @@
 import { Injectable } from '@angular/core';
-import {} from 'googlemaps';
-import { environment } from '../../../environments/environment';
-
-declare var window: any;
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
-  private static mapPromise: Promise<any>;
-  private map: google.maps.Map;
 
-  public get Map(): google.maps.Map {
-    return this.map;
-  }
-
-  public static loadGoogleMaps(): Promise<any> {
-    if (!MapService.mapPromise) {
-      MapService.mapPromise = new Promise(resolve => {
-        window.__onGapiLoaded = () => {
-          console.log('Gapi loaded');
-          resolve(window.gapi);
-        };
-        console.log('Loading google maps');
-        const el = document.createElement('script');
-        el.src = `https://maps.googleapis.com/maps/api/js?key=${
-          environment.mapsApiKey
-        }&callback=__onGapiLoaded`;
-        el.type = 'text/javascript';
-        document.getElementsByTagName('head')[0].appendChild(el);
-      });
-    }
-    return MapService.mapPromise;
-  }
-
-  public initGoogleMaps(
-    element: HTMLElement,
-    opts?: google.maps.MapOptions
-  ): Promise<void> {
-    return MapService.loadGoogleMaps().then(() => {
-      this.map = new google.maps.Map(element, opts);
-      console.log(this.map);
-    });
-  }
 }
