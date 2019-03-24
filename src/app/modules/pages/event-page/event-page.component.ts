227e5a739323cf7c7bb4d713e2e25b557bc00fb8
Hayrullah Behar <>
2019-03-24 11:00:46 +0100

    [HB] update events list in real time

--- src/app/modules/pages/event-page/event-page.component.ts
+++ src/app/modules/pages/event-page/event-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {AppColors} from '../../../models/colors.model';
-import {EventService} from '../../../shared/services/event.service';
-import {IEvent} from '../../../models';
+import { AppColors } from '../../../models/colors.model';
+import { EventService } from '../../../shared/services/event.service';
+import { IEvent } from '../../../models';
 import { MessageService } from 'src/app/shared/services/messenger.service';
 
 interface ICategory {
@@ -30,28 +30,28 @@ export class EventPageComponent implements OnInit {
       {
         name: 'music',
         color: AppColors.purple,
-        selected: true,
+        selected: true
       },
       {
         name: 'sport',
         color: AppColors.lime,
-        selected: true,
+        selected: true
       },
       {
         name: 'culture',
         color: AppColors.red,
-        selected: true,
+        selected: true
       },
       {
         name: 'shows',
         color: AppColors.magenta,
-        selected: true,
+        selected: true
       },
       {
         name: 'workshops',
         color: AppColors.yellow,
-        selected: true,
-      },
+        selected: true
+      }
     ];
 
     const children: Array<{ label: string; value: string }> = [];
@@ -61,24 +61,35 @@ export class EventPageComponent implements OnInit {
     this.listOfOption = children;
 
     this.events = [];
-    this.eventService.getAllEvents()
-      .then(data => {
-        const response = data.data;
+    this.eventService.getAllEvents().then(data => {
+      const response = data.data;
 
-        for (const tmp of response) {
-          const key = Object.keys(tmp)[0];
-          const event = tmp[key];
-          // tslint:disable-next-line
-          event['id'] = key;
-          this.events.push(event);
-        }
-
-      });
-
+      for (const tmp of response) {
+        const key = Object.keys(tmp)[0];
+        const event = tmp[key];
+        // tslint:disable-next-line
+        event["id"] = key;
+        this.events.push(event);
+      }
+    });
+    this.eventService.listenAllEvents().subscribe(({ data }) => {
+      if (data && data.length) {
+        const event = data;
+        const key = Object.keys(event[0])[0];
+        const e = event[0][key] as any;
+        e.id = key;
+        this.events.push(e);
+      }
+    });
+    this.eventService.onEventDelete().subscribe(({ data: { eventId } }) => {
+      console.log(eventId);
+      const filteredEvents = this.events.filter(({ id }) => id !== eventId);
+      this.events = filteredEvents;
+      console.log(this.events.length);
+    });
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   toggleCategory(event, category: ICategory) {
     category.selected = !category.selected;
@@ -87,5 +98,4 @@ export class EventPageComponent implements OnInit {
   showOnMap(item) {
     this.messengerService.sendMessage(item);
   }
-
 }
